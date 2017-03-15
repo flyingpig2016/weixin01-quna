2baf6802b1b2e64e54ffdb5303fb7c824a1aee40
flyingpig2016 <flyingpig2016@163.com>
2017-03-15 19:53:24 +0800

    Add files via upload

---
+++ pages/quna/quna.js
@@ -0,0 +1,29 @@
+ //quna.js
+//获取应用实例
+var app = getApp()
+Page({
+	data:{
+		
+	},
+	onLoad:function(options){
+		
+	},
+	onReady:function(){
+		
+	},
+	onShow:function(){
+		
+	},
+	onHide:function(){
+		
+	},
+	onUnload:function(){
+		
+	},
+	onPullDownRefresh:function(){
+		
+	},
+	onReachBottom:function(){
+		
+	}
+})		
